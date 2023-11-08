757321047a8d13985e1e31584148470a47d25155
Techbite <bonfacemwema7@gmail.com>
2023-11-08 12:36:46 +0300

    small changes

---
+++ app/src/main/java/com/example/module3/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.module3;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
